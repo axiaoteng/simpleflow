1a2478bc9f557fa9a64887e10fd4cb520beb39fe
lolizeppelin <lolizeppelin@gmail.com>
2017-09-05 17:42:03 +0800

    类型转化增加

--- simpleflow/types/dbconver.py
+++ simpleflow/types/dbconver.py
@@ -10,3 +10,12 @@ class SimpleFlowConverter(MySQLConverter):
 
     def _dict_to_mysql(self, value):
         return jsonutils.dumps(value)
+
+    def _list_to_mysql(self, value):
+        return jsonutils.dumps(value)
+
+    def _tuple_to_mysql(self, value):
+        return jsonutils.dumps(list(value))
+
+    def _set_to_mysql(self, value):
+        return jsonutils.dumps(list(value))
